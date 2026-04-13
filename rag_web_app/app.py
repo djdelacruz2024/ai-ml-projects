5d2b45c8fcdf92a8c35143c086e4039a09abc413
David De La Cruz <djdelacruz@miners.utep.edu>
2026-04-12 21:19:57 -0600

    Fix port binding for Rendder deployment

--- rag_web_app/app.py
+++ rag_web_app/app.py
@@ -77,5 +77,5 @@ def chat():
     response = chat_with_rag(user_message)
     return jsonify({'response': response})
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)

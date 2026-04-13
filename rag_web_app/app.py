0362f0168a36dd4e18fcc852fe08fa2146f28689
David De La Cruz <djdelacruz@miners.utep.edu>
2026-04-12 20:52:24 -0600

    Add requirements.txt and production config for deployment

--- rag_web_app/app.py
+++ rag_web_app/app.py
@@ -77,4 +77,5 @@ def chat():
     response = chat_with_rag(user_message)
     return jsonify({'response': response})
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
